e13aa287cd0be3a6d5ee380a5e037fdfc1dcb942
hashim-shehabdeen <hashim.sys@gmail.com>
2018-08-13 15:07:28 -0600

    Show the beach

---
+++ Beach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Beach.MvcApplication" Language="C#" %>
---
+++ Beach/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Beach.Startup))]
+namespace Beach
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
